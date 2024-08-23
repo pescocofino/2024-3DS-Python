f6433932f14f2b46cffba04018011efb40d0a74e
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-08-23 07:44:08 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+print("""
+
+
+▄▀█ █▀█ ▀█▀ ▀█▀ █▀▀ █▀▀ █░█
+█▀█ █▀▄ ░█░ ░█░ ██▄ █▄▄ █▀█
+""")
+
+print("1. Cadastrar Artista")
+print("2. Listar Projetos")
+print("3. Ativar Publicação")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção ", opcao_escolhida)
