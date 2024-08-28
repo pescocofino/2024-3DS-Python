8c84de9fcebdacca269f3f7da63874bd841bcfbd
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-08-28 12:00:04 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,51 +1,76 @@
-import os
+import os 
 
-def mostra_titulo():
+Artistas = []
+
+
+def exibir_titulo():
     print("""
+░░░░░██╗███╗░░░███╗  ██╗░░░██╗██╗██████╗░██████╗░░█████╗░░█████╗░░█████╗░██████╗░██╗░█████╗░
+░░░░░██║████╗░████║  ██║░░░██║██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗
+░░░░░██║██╔████╔██║  ╚██╗░██╔╝██║██║░░██║██████╔╝███████║██║░░╚═╝███████║██████╔╝██║███████║
+██╗░░██║██║╚██╔╝██║  ░╚████╔╝░██║██║░░██║██╔══██╗██╔══██║██║░░██╗██╔══██║██╔══██╗██║██╔══██║
+╚█████╔╝██║░╚═╝░██║  ░░╚██╔╝░░██║██████╔╝██║░░██║██║░░██║╚█████╔╝██║░░██║██║░░██║██║██║░░██║
+░╚════╝░╚═╝░░░░░╚═╝  ░░░╚═╝░░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚═╝\n""")
 
 
-    ▄▀█ █▀█ ▀█▀ ▀█▀ █▀▀ █▀▀ █░█
-    █▀█ █▀▄ ░█░ ░█░ ██▄ █▄▄ █▀█
-    """)
-
-def mostra_escolhas():
-    print("1. Cadastrar Artista")
-    print("2. Listar Projetos")
-    print("3. Ativar Publicação")
-    print("4. Sair\n")
-
-def escolhe_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
-
-    def finaliza_programa():
-        os.system("cls")
-        print("Finalizando o programa\n")
-
-    def opcao_invalida():
-        print("Esse caracter não e permitido\n")
-        input("Aperte qualquer tecla para voltar")
-        main()
-    try:
-            opcao_escolhida = int(input("Escolha uma opção: "))
-
-            if opcao_escolhida == 1:
-                print("Você escolheu Cadastrar Artista.")
-            elif opcao_escolhida == 2:
-                print("Você escolheu Listar seus projetos.")
-            elif opcao_escolhida == 3:
-                print("Você escolheu Ativar sua publicação.")
-            elif opcao_escolhida == 4:
-                finaliza_programa()
-            else:
-                opcao_invalida()
-    except:
-     opcao_invalida()
-
-    def main():
-        mostra_titulo()
-        mostra_escolhas()
-        escolhe_opcao()
-
-    if__name__ == "__main__"
+def cadastra_artistas():
+    os.system("cls")
+    print("Cadastro de novos artistas\n")
+    nome_do_artista = input("Digite o nome do artista que deseja cadastrar: ")
+    Artistas.append(nome_do_artista)
+    print(f"O artista {nome_do_artista} foi cadastrado com sucesso\n")
+    input("Digite uma tecla para voltar ao menu principal")
     main()
+
+def exibir_opcao():
+    print('1. Cadastrar peça')
+    print('2. Listar lista')
+    print('3. comprar peças')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print("Finalizando programa")
+
+def opcao_invalida():
+    print("Opçao Invalida")
+    input("Aperte um botão para retornar")
+    main()
+
+def mostra_artistas():
+    os.siystem("cls")
+    print("Artistas publícos\n")
+    
+    for Artistas in Artistas:
+        print(f"-{Artistas}")
+
+    input("\nDigite uma tecla para voltar ao menu principal")
+
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+    try:
+        if opcao_escolhida == 1:
+            print('Cadastrar peças')
+        elif opcao_escolhida == 2:
+            
+        elif opcao_escolhida == 3:
+            print('comprar peças')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
+    exibir_titulo()
+    exibir_opcao()
+    escolher_opcao()
+
+if __name__ == "__main__":
+    main()
+
+
 
